126626e89d432fc256dcad7d2d54825e89a87de2
TheblueMan003 <31632552+TheblueMan003@users.noreply.github.com>
2022-03-07 08:46:25 +0100

    first commit

---
+++ src/main/scala/Parsing/Lexer.scala
@@ -0,0 +1,10 @@
+package parsing
+
+import java.io.File
+import Tokens._
+
+object Lexer{
+    def parse(text: Iterator[Char]):List(Token){
+        
+    }
+}
---
+++ src/main/scala/Parsing/Token.scala
@@ -0,0 +1,19 @@
+package parsing
+
+object Tokens {
+  final case class KeywordToken(value: String) extends Token
+  final case class IdentifierToken(name: String) extends Token
+  final case class PrimTypeToken(value: String) extends Token
+
+  final case class IntLitToken(value: Int) extends Token
+  final case class FloatLitToken(value: Float) extends Token
+  final case class StringLitToken(value: String) extends Token
+  final case class BoolLitToken(value: Boolean) extends Token
+
+  final case class DelimiterToken(value: String) extends Token
+  final case class OperatorToken(name: String) extends Token
+  final case class CommentToken(text: String) extends Token
+  final case class SpaceToken() extends Token
+  final case class ErrorToken(content: String) extends Token
+  final case class EOFToken() extends Token
+}
