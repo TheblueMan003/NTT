fec43bc6dbf2750e8811d2aabcf4130adc3ad6d1
Christian Fattebert <christian.fattebert@epfl.ch>
2022-03-08 13:13:38 +0100

    replace call with implicit

--- src/main/scala/Parsing/Parser.scala
+++ src/main/scala/Parsing/Parser.scala
@@ -4,9 +4,10 @@ import ast.Tree
 import Tokens._
 import utils.Context
 import utils.VariableOwner
+import scala.collection.mutable.ArrayBuffer
 
 object Parser{
-    def functionDiscovery(text: TokenBufferedIterator, context: Context): Unit = {
+    def functionDiscovery()(implicit text: TokenBufferedIterator, context: Context): Unit = {
         val c = text.take()
         c match{
             case KeywordToken("to") => {
@@ -24,27 +25,37 @@ object Parser{
                 context.addFunction(name, List(), body)
             }
             case KeywordToken("globals") => {
-                parseVariablesGroup(text).map(
+                parseVariablesGroup().map(
                     context.addVariable(_, VariableOwner.Global())
                 )
             }
             case KeywordToken("turtles-own") => {
-                parseVariablesGroup(text).map(
+                parseVariablesGroup().map(
                     context.addVariable(_, VariableOwner.TurtlesOwned())
                 )
             }
             case KeywordToken("patches-own") => {
-                parseVariablesGroup(text).map(
+                parseVariablesGroup().map(
                     context.addVariable(_, VariableOwner.PatchesOwned())
                 )
             }
         }
         if (text.hasNext()){
-            functionDiscovery(text, context)
+            functionDiscovery()
         }
     }
 
-    def parseVariablesGroup(text: TokenBufferedIterator): List[String] = {
+    def parseFunctionBody()(implicit text: TokenBufferedIterator) = {
+        val body = ArrayBuffer[Tree]()
+        while(text.hasNext()){
+            body.addOne(parseCallOrVariable())
+        }
+    }
+
+    def parseCallOrVariable()(implicit text: TokenBufferedIterator): Tree = ???
+    def parseExpression()(implicit text: TokenBufferedIterator): Tree = ???
+
+    def parseVariablesGroup()(implicit text: TokenBufferedIterator): List[String] = {
         text.requierToken(DelimiterToken("["))
         text.setStart()
         text.takeWhile(x => x match {
