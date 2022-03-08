0ecf82e364129f94ba9b9260c2d7c40d37dac55c
Christian Fattebert <christian.fattebert@epfl.ch>
2022-03-08 12:54:15 +0100

    _

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,7 +5,7 @@ import parsing.Parser
 import utils.Context
 
 object Main extends App {
-  val r1 = Lexer.parse(new StringBufferedIterator("globals [ test fuck ]"), new TokenBuffer())
+  val r1 = Lexer.parse(new StringBufferedIterator("globals [ test dummy ]"), new TokenBuffer())
   println(r1.list)
   val con = new Context()
   val r2 = Parser.functionDiscovery(r1.toBuffer(), con)
