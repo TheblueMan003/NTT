b86e53d331981a440cc1741642abc0a8100f8875
TheblueMan003 <31632552+TheblueMan003@users.noreply.github.com>
2022-04-28 10:42:34 +0200

    partial commit

---
+++ src/main/resources/output/agent.scala
@@ -0,0 +1,8 @@
+class agent{
+	var go_m2 : Int = 0
+	var go_m : Float = 0
+	def go():Unit = {
+		go_m2 = 5
+		go_m = 2.0
+	}
+}
---
+++ src/main/resources/output/turtle.scala
@@ -0,0 +1,33 @@
+class turtle{
+	var test4 : Int = 0
+	var other : Int = 0
+	var test : Int = 0
+	var arg_m : Float = 0
+	var test3-test? : Int = 0
+	var argtest_m : Float = 0
+	def arg():Unit = {
+		arg_m = 5.0
+		arg_m = 0
+		right(0)
+		fw(1)
+		argtest(5.0)
+		argtest(5)
+		show("test")
+	}
+	def argtest():Unit = {
+		argtest_m = 5
+		argtest_m = 0.0
+
+		if((value == 0.0)){
+			if((argtest_m == 0)){
+				right(5)
+			}
+		}
+		else if((value > 2.0)){
+			left(5)
+		}
+		else{
+			fw(5)
+		}
+	}
+}
